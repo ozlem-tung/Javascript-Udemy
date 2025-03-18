c035656cee1fdb63daf2613e6aca4214dac3402b
Özlem Tunğ <ozlem.tng.07@gmail.com>
2025-03-18 17:31:43 +0300

    attack buttons and control system

---
+++ MonsterKiller/assets/scripts/app.js
@@ -0,0 +1,38 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
+let chosenMaxLife = 100;
+
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode == 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if ((mode = 'STRONG_ATTACK')) {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('YOU DRAW !');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('YOU WON !');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('YOU LOST !');
+  }
+}
+
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
