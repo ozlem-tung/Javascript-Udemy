690b93ece117e45da29c400d0014a96ee655ec2f
Özlem Tunğ <ozlem.tng.07@gmail.com>
2025-07-19 14:13:26 +0300

    dom assigment added for practice

---
+++ dom-assigment/assignment.js
@@ -0,0 +1,11 @@
+const task1 = document.getElementById('task-1');
+task1.style.color = 'white';
+task1.style.backgroundColor = 'black';
+
+
+const title=document.querySelector('title');
+title.textContent='Assignment - Solved!';
+
+
+const h1=document.querySelector('h1');
+h1.textContent='Assignment - Solved!';
